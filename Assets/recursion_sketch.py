279880ef0f1ef64a814674a01cc07b64e0b2569f
Mark <wehnsdaefflae@web.de>
2020-01-13 18:58:37 +0100

    python sketch

--- Assets/recursion_sketch.py
+++ Assets/recursion_sketch.py
@@ -1,29 +1,18 @@
-class Brane():
-	def __init__(location: int, subbranes: Optional[Tuple[Brane, ...]] = None):
-		self.location = location
-		if subbranes is None:
-			self.dim = 0
-
-		else:
-			no_subbranes = len(subbranes)
-			assert 1 < no_subbranes
-			assert no_subbranes % 2 == 0
-			self.dim = no_subbranes // 2
-			assert all(x.dim == self.dim - 1 for x in subbranes)
-
-		self.subbranes = subbranes
+from typing import Tuple, Dict
 
 
-	def middle() -> Sequence[int]:
-		if brane.dim == 0:
-			return [self.location]
+def noise(matrix: Dict[Tuple[int, ...], int], point_origin: Tuple[int, ...], size: int):
+	dim = len(point_origin)
+	value_a = matrix[point_origin]
 
-		elif brane.dim == 1:
-			return [sum(x.location for x in brane.subbranes) / len(brane.subbranes)]
+	for i in range(dim):
+		point_b = list(point_origin)
+		point_mid = list(point_origin)
 
-		else:
-			for each_brane in brane.subbranes:
-				i = interpolate(each_brane)
-				coordinate.append(i)
+		for j in range(i + 1):
+			point_b[j] = size
+			point_mid[j] = (point_origin[j] + size) // 2
 
-
+		value_b = matrix[tuple(point_b)]
+		value_mid = (value_a + value_b) // 2
+		matrix[tuple(point_mid)] = value_mid
