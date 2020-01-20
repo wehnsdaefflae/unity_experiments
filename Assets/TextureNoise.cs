381c66ea8e74cfdcc9337d108e2843afcabbe0d3
Mark <wehnsdaefflae@web.de>
2020-01-20 18:49:14 +0100

    debugging

--- Assets/TextureNoise.cs
+++ Assets/TextureNoise.cs
@@ -4,25 +4,29 @@ using UnityEngine.Assertions;
 using System.Linq;
 
 public class TextureNoise : MonoBehaviour {
-    public int size = 512;
+    public int size = 128;
     public float randomness = .1f;
     private NoiseContainer NoiseVolume;
     private MyNoise myNoise;
     private MyNoiseNew myNoiseNew;
 
 
-    /*
-    public override Texture2D GetTexture(int layer, NoiseVolume noiseVolume) {
-        int[] shape = this.NoiseVolume.shape;
-        
+    private static Texture2D GetTexture(NoiseContainer noiseVolume, params int[] layer) {
+        int[] shape = noiseVolume.shape;
+        int dimensions = layer.Length;
+        Assert.AreEqual(dimensions + 2, shape.Length);
 
         Texture2D texture = new Texture2D(shape[0], shape[1]);
 
+        int[] coordinates = new int[shape.Length];
+        for (int i = 2; i < coordinates.Length; i++) coordinates[i] = layer[i];
         float value;
         Color color;
         for (int x = 0; x < shape[0]; x++) {
+            coordinates[0] = x;
             for (int y = 0; y < shape[1]; y++) {
-                value = this.Get(x, y, layer);
+                coordinates[1] = y;
+                value = noiseVolume.Get(coordinates);
                 color = new Color(value, value, value);
                 texture.SetPixel(x, y, color);
             }
@@ -33,7 +37,6 @@ public class TextureNoise : MonoBehaviour {
 
         return texture;
     }
-    */
     
 
     void Start() {
@@ -45,8 +48,8 @@ public class TextureNoise : MonoBehaviour {
         int width = size + (wrappedDimensions.Contains(0) ? 0 : 1);
         int height = size + (wrappedDimensions.Contains(1) ? 0 : 1);
         
-        // this.NoiseVolume = new NoiseVolume(new int[] { width, height, 1 });
-        this.NoiseVolume = new NoiseTextureGray(width, height);
+        this.NoiseVolume = new NoiseVolume(new int[] { width, height, this.size });
+        // this.NoiseVolume = new NoiseTextureGray(width, height);
 
         // this.myNoise = new MyNoise(this.NoiseVolume, this.size, wrappedDimensions);
         this.myNoiseNew = new MyNoiseNew(this.NoiseVolume, this.size, wrappedDimensions);
@@ -56,10 +59,12 @@ public class TextureNoise : MonoBehaviour {
         Renderer renderer = GetComponent<Renderer>();
 
         // this.myNoise.Generate(this.randomness);
-        this.myNoiseNew.Generate(this.randomness);
+        if (this.myNoiseNew != null) this.myNoiseNew.Generate(this.randomness);
 
         // renderer.material.mainTexture = TextureNoise.CubeToTexture((NoiseVolume) this.NoiseVolume, 0);
-        renderer.material.mainTexture = ((NoiseTextureGray) this.NoiseVolume).GetTexture();
+        // renderer.material.mainTexture = ((NoiseTextureGray) this.NoiseVolume).GetTexture();
+
+        if (this.NoiseVolume != null) renderer.material.mainTexture = TextureNoise.GetTexture(this.NoiseVolume, 0);
     }
 
 }
