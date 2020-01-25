d21fd716d9e7f51326897ab3a6cf05ae3de2dad5
Mark <wehnsdaefflae@web.de>
2020-01-25 23:01:14 +0100

    major refactoring

---
+++ Assets/Scripts/Editor/EditorMapGenerator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (UpdateTexture))]
+public class EditorMapGenerator : Editor {
+
+    public override void OnInspectorGUI() {
+        UpdateTexture updateTexture = (UpdateTexture)target;
+
+        updateTexture.MyAwake();
+
+        if (DrawDefaultInspector() && updateTexture.autoUpdate) updateTexture.MyUpdate();
+
+        if (GUILayout.Button("Generate")) updateTexture.MyUpdate();
+
+    }
+}
---
+++ Assets/Scripts/Tools/MathTools.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets {
+    public class MathTools {
+        public static int Modulo(int a, int b) {
+            return ((a % b) + b) % b;
+        }
+
+        public static float Randomize(float value, float randomness) {
+            return MathTools.Randomize(value, randomness, 0f, 1f);
+        }
+
+        public static float Randomize(float value, float randomness, float minValue, float maxValue) {
+            return Mathf.Max(minValue, Mathf.Min(maxValue, value + UnityEngine.Random.Range(-randomness, randomness)));
+        }
+
+    }
+}
